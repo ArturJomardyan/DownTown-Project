8dd64ec7e3bb3927c7f133a7a527f63ef852f02f
Artur Jomardyan <jomardyan19@gmail.com>
2022-03-07 15:37:13 +0400

    3 Topic task 1

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-var b = 5;
-let x = 15;
+console.log(document.body.children[0]); // Напишите код, как получит Div
+console.log(document.body.children[1]); // Напишите код, как получит ul
+console.log(document.body.children[1].children[1]); // второй <li> (с именем Пит)?
