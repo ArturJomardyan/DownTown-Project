679191b0b6ebf564064d88582716ffac5a9d7a23
Artur Jomardyan <jomardyan19@gmail.com>
2022-03-08 23:26:10 +0400

    Tasks

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+// Topic 3 task 1
+
 // console.log(document.body.children[2].nextSibling); // Напишите код, как получит Div
 // console.log(document.body.children[1]); // Напишите код, как получит ul
 // console.log(document.body.children[1].children[1]); // второй <li> (с именем Пит)?
 
-let table = document.getElementsByClassName("table")[0];
+// Topic 3 task 3
 
-for (let index = 0; index < table.rows.length; index++) {
+// let table = document.getElementsByClassName("table")[0];
 
-    for (let index1 = 0; index1 < table.rows[index].cells.length; index1++) {
-        if (index = index1) {
-            table.rows[index].cells[index1].style.backgroundColor = "red"
-        }
-    }
-}
+// for (let index = 0; index < table.rows.length; index++) {
 
-console.log(table)
+//     for (let index1 = 0; index1 < table.rows[index].cells.length; index1++) {
+//         if (index = index1) {
+//             table.rows[index].cells[index1].style.backgroundColor = "red"
+//         }
+//     }
+// }
+
+// console.log(table)
