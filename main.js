e626f19a21a7ea6ebe94f5abc918f9dab4f0592c
Artur Jomardyan <jomardyan19@gmail.com>
2022-03-08 23:24:44 +0400

    Tasks

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-console.log(document.body.children[0]); // Напишите код, как получит Div
-console.log(document.body.children[1]); // Напишите код, как получит ul
-console.log(document.body.children[1].children[1]); // второй <li> (с именем Пит)?
+// console.log(document.body.children[2].nextSibling); // Напишите код, как получит Div
+// console.log(document.body.children[1]); // Напишите код, как получит ul
+// console.log(document.body.children[1].children[1]); // второй <li> (с именем Пит)?
+
+let table = document.getElementsByClassName("table")[0];
+
+for (let index = 0; index < table.rows.length; index++) {
+
+    for (let index1 = 0; index1 < table.rows[index].cells.length; index1++) {
+        if (index = index1) {
+            table.rows[index].cells[index1].style.backgroundColor = "red"
+        }
+    }
+}
+
+console.log(table)
