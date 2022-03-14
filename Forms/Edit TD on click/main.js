8a4e54293f1d35e9e9bb35b484ee505f0c3badf3
ArturJomardyan <91560836+ArturJomardyan@users.noreply.github.com>
2022-03-14 23:05:28 +0400

    Update main.js

--- Forms/Edit TD on click/main.js
+++ Forms/Edit TD on click/main.js
@@ -1,14 +1,57 @@
-let table = document.getElementsByTagName("tBody");
-//event.target.offsetHeight
-//event.target.offsetWidth
-//offsetY
-//offsetX
+// let rootTable = document.getElementsByTagName("table");
+
+    // rootTable.addEventListener("click",f)
+    // rootTable.addEventListener("click",ChekEdite);
 
 function f(event) {
-    let target = event.target
-        // target.style.display = "none"
-    let textarea = document.createElement("textarea");
-    textarea.innerText = target.innerHTML
-    textarea.style.width = target.offsetWidth
-    textarea.style.height = target.offsetHeight
+    
+   let descendantTable = event.currentTarget.querySelectorAll("*");
+   let activCellChek = Array.from(descendantTable).filter(el => el.tagName === "TEXTAREA").length;
+   
+    if(!activCellChek){
+        let target = event.target
+        let textarea = document.createElement("textarea");
+    
+        textarea.style.width = target.offsetWidth + "px"
+        textarea.style.height = target.offsetHeight + "px"
+        textarea.style.resize = "none"
+    
+        textarea.innerText = target.innerHTML
+        target.innerHTML = ""
+        target.style.all = "unset"
+        target.append(textarea);
+       
+        createButtons(target)
+        
+     }else if(activCellChek && (event.target.tagName !== "BUTTON" || event.target.tagName !== "TEXTAREA"))return
+     else if(event.target.tagName === "BUTTON" && event.target.innerHTML === "ok"){
+      console.log("ok button clicked");
+      }else{
+        console.log(event.target.tagName);
+        if(event.target.innerHTML === "ok")console.log("okButton");
+    }
+
+    
+   
+}
+
+function createButtons(root){
+   
+    let okBtn = document.createElement("button");
+    okBtn.innerText = "ok"
+    okBtn.style.position = 'absolute';
+    okBtn.style.left = root.offsetLeft + root.offsetWidth / 2 + "px"
+    okBtn.style.top =  root.offsetTop + root.offsetHeight +  "px"
+    root.prepend(okBtn)
+
+    let cnceleBtn = document.createElement("button");
+    cnceleBtn.innerText = "cancale"
+    cnceleBtn.style.position = 'absolute';
+    cnceleBtn.style.top =  root.offsetTop + root.offsetHeight +  "px"
+    root.prepend(cnceleBtn)
+
+}
+
+function ChekEdite(event){
+    console.log(event.currentTarget);
 }
