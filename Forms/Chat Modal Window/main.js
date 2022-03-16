9fe832a56c2b2080034f6ef593ac63d859c39511
Artur Jomardyan <jomardyan19@gmail.com>
2022-03-16 17:46:59 +0400

    chat

--- Forms/Chat Modal Window/main.js
+++ Forms/Chat Modal Window/main.js
@@ -1,12 +1,32 @@
-function open_and_close_Modal(event) {
-    if (event.target.innerText = "Open Modal Window") {
-        console.log(document.forms[0]);
-        document.forms[0].style.display = 'block'
-        document.forms[0].previousElementSibling.style.display = 'block'
-    } else if (event.target.innerText = "cancele") {
-        event.target.parentElement.style.display = 'none'
-        event.target.previousElementSibling.style.display = 'none'
+function open_modal(event) {
+    document.forms[0].style.display = 'block'
+    document.forms[0].previousElementSibling.style.display = 'block'
+}
+
+function close_modal(event) {
+    event.target.parentElement.style.display = 'none'
+    event.target.parentElement.previousElementSibling.style.display = 'none'
+    alert("null");
+}
+
+let textarea = document.getElementsByTagName("textarea")[0];
+
+function submitFunc() {
+    alert(textarea.value);
+}
+
+document.onkeydown = function(e) {
+    if (document.forms[0].style.display === "block" && e.key == 'Escape') {
+        document.forms[0].style.display = "none"
+        document.forms[0].previousElementSibling.style.display = 'none'
+        alert("null");
+    } else if (document.forms[0].style.display === "block" && e.key == 'Enter') {
+        document.forms[0].style.display = "none"
+        document.forms[0].previousElementSibling.style.display = 'none'
+        submitFunc();
+
     } else {
         return
     }
-}
+
+};
