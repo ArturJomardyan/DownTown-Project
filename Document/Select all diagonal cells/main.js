58295f9a494bc7dda01aa1d6f7a0836b90bbfd3a
Artur Jomardyan <jomardyan19@gmail.com>
2022-03-26 20:04:19 +0400

    Document bug fix

--- Document/Select all diagonal cells/main.js
+++ Document/Select all diagonal cells/main.js
@@ -1,13 +1,9 @@
 let table = document.getElementsByClassName("table")[0];
 
-
-for (let index = 0; index < table.rows.length; index++) {
-
-    for (let index1 = 0; index1 < table.rows[index].cells.length; index1++) {
-        if (index = index1) {
-            table.rows[index].cells[index1].style.backgroundColor = "red"
-        }
-    }
+let tableRows = table.rows;
+for (const row of tableRows) {
+    let rowIndex = row.rowIndex;
+    row.cells[rowIndex].style.backgroundColor = "red"
 }
 
 console.log(table)
