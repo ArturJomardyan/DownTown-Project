0f8a366cdd07f63996e1391256da75778439d530
ArturJomardyan <91560836+ArturJomardyan@users.noreply.github.com>
2022-03-28 21:22:52 +0400

    Update main.js
    table sort

--- Document/Sort Table/main.js
+++ Document/Sort Table/main.js
@@ -1,10 +1,17 @@
 let table = document.getElementsByTagName("table")[0];
-
+let count = 1;
 table.addEventListener("click", function (event) {
-    let index = event.target.cellIndex
-    let sortedRows = Array.from(table.rows)
-        .slice(1)
-        .sort((rowA, rowB) => rowA.cells[index].innerHTML > rowB.cells[index].innerHTML ? 1 : -1);
-
+    let index = event.target.cellIndex;
+    let sortedRows
+    if (count % 2 !== 0) {
+        sortedRows = Array.from(table.rows)
+            .slice(1)
+            .sort((rowA, rowB) => rowA.cells[index].innerHTML > rowB.cells[index].innerHTML ? 1 : -1);
+    } else {
+        sortedRows = Array.from(table.rows)
+            .slice(1)
+            .sort((rowA, rowB) => rowA.cells[index].innerHTML < rowB.cells[index].innerHTML ? 1 : -1);
+    }
+    count++
     table.tBodies[0].append(...sortedRows);
 })
