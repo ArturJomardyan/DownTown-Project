73243e1ec6fb09cb002e84c2ebac1333968af82b
Artur Jomardyan <jomardyan19@gmail.com>
2022-03-28 17:26:56 +0400

    sort table

--- Document/Sort Table/main.js
+++ Document/Sort Table/main.js
@@ -2,12 +2,9 @@ let table = document.getElementsByTagName("table")[0];
 
 table.addEventListener("click", function (event) {
     let index = event.target.cellIndex
-    let arr = [];
-    for (let i = 1; i < table.rows.length; i++) {
-        arr.push(table.rows[i].children[index].innerHTML);
-    }
-    table.rows[0].children[index].innerText !== "Возраст" ? arr = arr.sort() : arr = arr.sort((a, b) => a - b);
-    for (let i = 1; i < table.rows.length; i++) {
-        arr.push(table.rows[i].children[index].innerHTML = arr[i - 1]);
-    }
+    let sortedRows = Array.from(table.rows)
+        .slice(1)
+        .sort((rowA, rowB) => rowA.cells[index].innerHTML > rowB.cells[index].innerHTML ? 1 : -1);
+
+    table.tBodies[0].append(...sortedRows);
 })
