5880015c7866cdf40f431a57e5119a6a85273313
ArturJomardyan <91560836+ArturJomardyan@users.noreply.github.com>
2022-03-28 22:55:11 +0400

    Update main.js
    Bug fix

--- Document/Sort Table/main.js
+++ Document/Sort Table/main.js
@@ -2,15 +2,11 @@ let table = document.getElementsByTagName("table")[0];
 let count = 1;
 table.rows[0].addEventListener("click", function (event) {
     let index = event.target.cellIndex;
-    let sortedRows
+    let sortedRows = Array.from(table.rows).slice(1)
     if (count % 2 !== 0) {
-        sortedRows = Array.from(table.rows)
-            .slice(1)
-            .sort((rowA, rowB) => rowA.cells[index].innerHTML > rowB.cells[index].innerHTML ? 1 : -1);
+        sortedRows.sort((rowA, rowB) => rowA.cells[index].innerHTML > rowB.cells[index].innerHTML ? 1 : -1);
     } else {
-        sortedRows = Array.from(table.rows)
-            .slice(1)
-            .sort((rowA, rowB) => rowA.cells[index].innerHTML < rowB.cells[index].innerHTML ? 1 : -1);
+        sortedRows.sort((rowA, rowB) => rowA.cells[index].innerHTML < rowB.cells[index].innerHTML ? 1 : -1);
     }
     count++
     table.tBodies[0].append(...sortedRows);
